82d3d511e770cdb2d52e6d5d7fb500c76e327673
LouvartY <152400789+LouvartY@users.noreply.github.com>
2023-12-13 14:43:43 +0100

    Upload puttftp

---
+++ puttftp.c
@@ -0,0 +1,17 @@
+#include "TFTP.h"
+
+
+void error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <host> <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *host = argv[1];
+    const char *file = argv[2];
+}
